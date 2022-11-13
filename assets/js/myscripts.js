3e6569e427cb01cda8784745d69aae4fa11112b4
EdyCartof <67964233+EdyCartof@users.noreply.github.com>
2022-11-13 12:37:04 +0200

    Add files via upload

--- assets/js/myscripts.js
+++ assets/js/myscripts.js
@@ -1,1 +1,13 @@
-
+function initMap() {
+    const uluru = { lat: 45.65634431752302, lng: 25.59872733562484 };
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 10,
+      center: uluru,
+    });
+    const marker = new google.maps.Marker({
+      position: uluru,
+      map: map,
+    });
+  }
+  
+  window.initMap = initMap;
